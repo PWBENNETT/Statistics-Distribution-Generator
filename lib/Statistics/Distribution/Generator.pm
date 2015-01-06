237d18986f6b117b0641d8d3047f72f7b8f3d41e
Paul Bennett <paul.w.bennett@gmail.com>
2015-01-06 03:41:48 -0500

    pod

--- lib/Statistics/Distribution/Generator.pm
+++ lib/Statistics/Distribution/Generator.pm
@@ -4,6 +4,7 @@ use 5.018;
 use utf8;
 use overload (
     '0+' => '_render',
+    '""' => '_render',
     '|' => '_add_alternative',
     'x' => '_add_dimension',
     fallback => 0,
@@ -214,3 +215,116 @@ sub Statistics::Distribution::Generator::gamma::_render {
 
 1;
 
+__END__
+
+=head1 NAME
+
+Statistics::Distribution::Generator - A way to compose complicated probability functions
+
+=head1 VERSION
+
+Version 0.001
+
+=head1 SYNOPSIS
+
+    use Statistics::Distribution::Generator qw( :all );
+    my $g = gaussian 3, 1;
+    say $g; # something probably between -3 and 9, but probably about 2 .. 4-ish
+    my $cloud = (gaussian 0, 1 x gaussian 0, 1 x gaussian 0, 1);
+    say @$cloud; # a 3D vector almost certainly within (+/- 6, +/- 6, +/- 6) and probably within (+/- 3, +/- 3, +/- 3)
+    my $combo = (gaussian 100, 15 | uniform 0, 200); # one answer with an equal chance of being picked from either distribution
+
+=head1 DESCRIPTION
+
+This module allows you to bake together multiple "simple" probability distributions into a more complex random number generator.
+
+=head1 EXPORTABLE FUNCTIONS
+
+=over
+
+=item gaussian MEAN, SIGMA
+
+Gaussian Normal Distribution
+
+=back
+
+=over
+
+=item uniform MIN, MAX
+
+A uniform distribution, with equal chance of any n where MIN Z<<>= n Z<<> MAX
+
+=back
+
+=over
+
+=item logistic
+
+Standard Logistic Distribution
+
+=back
+
+=over
+
+=item supplied VALUE
+
+=item supplied CALLBACK
+
+Allows the caller to supply either a constant VALUE which will always be returned, or a coderef CALLBACK that may use any algorithm you like to generate a random number
+
+=back
+
+=item gamma ORDER, SCALE
+
+Gamma Distribution
+
+The distribution function is
+
+    p(x) dx = {1 \over \Gamma(a) b^a} x^{a-1} e^{-x/b} dx
+    for x > 0.
+
+=back
+
+=head1 OVERLOADED OPERATORS
+
+=over
+
+=item x
+
+Allows you to compose multi-dimensional random vectors.
+
+    $randvect = $foo x $bar x $baz; # generate a three-dimensional vector
+
+=back
+
+=over
+
+=item |
+
+Allows you to pick a single (optionally weighted) generator from some set of generators.
+
+    $cointoss = supplied 0 | supplied 1; # fair 50:50 result of either 0 or 1
+
+=back
+
+=head1 AUTHOR
+
+The main body of this work is by Paul W Bennett E<paul.w.bennett@gmail.com>
+
+The idea of composing probabilities together comes from a paper by B<TODO: CITE THE PAPER HERE>
+
+The implementation of the Gamma Distribution is by Nigel Wetters Gourlay.
+
+=head1 CAVEATS
+
+Almost no error checking is done. Garbage in I<will> result in garbage out.
+
+=head1 TODO
+
+Build in more probability density functions.
+
+=head1 LICENSE
+
+Artistic 2.0
+
+=cut
